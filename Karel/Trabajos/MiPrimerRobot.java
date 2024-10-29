3695d97dde1025fccc67ff6660d25cb63e8dc097
SaraCmque <saramanrique2016@gmail.com>
2024-10-28 19:23:36 -0500

    Ejercicio 4 completado

--- Karel/Trabajos/MiPrimerRobot.java
+++ Karel/Trabajos/MiPrimerRobot.java
@@ -1,58 +1,44 @@
 import kareltherobot.*;
 import java.awt.Color;
 
-public class MiPrimerRobot implements Directions
-{
-    public static void main(String [] args)
-    {
-        // Usamos el archivo que creamos del mundo
+// Definición de la clase Racer que extiende Robot y permite ejecutar en un hilo
+class Racer extends Robot implements Runnable {
+    public Racer(int street, int avenue, Direction direction, int beepers, Color color) {
+        super(street, avenue, direction, beepers, color);
+        World.setupThread(this); // Configuración del hilo para el robot
+    }
+
+    // Método que define el recorrido del robot
+    public void race()
+        { while(! nextToABeeper())
+        move();
+        pickBeeper();
+        turnOff();
+        }
+
+    // Método que activa el hilo y ejecuta el recorrido
+    public void run() {
+        race();
+    }
+}
+
+public class MiPrimerRobot implements Directions { // Implementa Directions para acceder a las constantes de dirección
+    public static void main(String[] args) {
+        // Configuramos el mundo
         World.readWorld("Mundo.kwld");
         World.setVisible(true);
 
-        // Coloca el primer robot en la posición inicial del mundo (1,1),
-        // mirando al Este, sin ninguna sirena.
-        Robot Karel = new Robot(1, 1, East, 0);
+        // Crear el primer robot en la posición inicial
+        Racer first = new Racer(1, 1, East, 0, Color.GREEN);
 
-        // Coloca el segundo robot en la misma posición que el primer robot
-        // y le asigna el color azul.
-        Robot Azul = new Robot(1, 1, East, 0, Color.blue);
+        // Crear el segundo robot en la misma posición con color azul
+        Racer second = new Racer(2, 1, East, 0, Color.BLUE);
+        // Crear hilos para los robots
+        Thread thread1 = new Thread(first);
+        Thread thread2 = new Thread(second);
 
-        // Mover los robots alternadamente 4 pasos
-        Karel.move();
-        Azul.move();
-        Karel.move();
-        Azul.move();
-        Karel.move();
-        Azul.move();
-        Karel.move();
-        Azul.move();
-
-        // Solo el primer robot recoge los 5 beepers
-        Karel.pickBeeper();
-        Karel.pickBeeper();
-        Karel.pickBeeper();
-        Karel.pickBeeper();
-        Karel.pickBeeper();
-
-        // Girar a la izquierda y salir de los muros alternadamente
-        Karel.turnLeft();
-        Azul.turnLeft();
-        Karel.move();
-        Azul.move();
-        Karel.move();
-        Azul.move();
-
-        // Solo el primer robot coloca los beepers fuera de los muros
-        Karel.putBeeper();
-        Karel.putBeeper();
-        Karel.putBeeper();
-        Karel.putBeeper();
-        Karel.putBeeper();
-
-        // Moverse a otra posición y apagar ambos robots
-        Karel.move();
-        Azul.move();
-        Karel.turnOff();
-        Azul.turnOff();
+        // Iniciar los hilos para que ambos robots comiencen en paralelo
+        thread1.start();
+        thread2.start();
     }
 }
