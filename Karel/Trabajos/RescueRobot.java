7a7bc06cdd4f369ebae926741a22669202ab41cc
Daviddgp04 <daviddgp04@hotmail.com>
2024-11-17 16:51:26 -0500

    No two robots in the same position

--- Karel/Trabajos/RescueRobot.java
+++ Karel/Trabajos/RescueRobot.java
@@ -157,11 +157,15 @@ class RescueRobot extends Robot implements Runnable {
 
     public void rescuePeople() {
         synchronized (manager) {
-            while (nextToABeeper() && rescuedPeople < MAX_PEOPLE) {
+            int temp=this.sharedGrid.getValue((this.getStreet()-1) * 2, (this.getAvenue()-1) * 2);
+            while (nextToABeeper() && rescuedPeople < MAX_PEOPLE && this.sharedGrid.getValue((this.getStreet()-1) * 2, (this.getAvenue()-1) * 2) != -1) {
+                this.sharedGrid.setValue((this.getStreet()-1) * 2, (this.getAvenue()-1) * 2,-1);
                 pickBeeper();
                 rescuedPeople++;
                 log("Rescued a person. Total rescued: " + rescuedPeople);
             }
+            this.sharedGrid.setValue((this.getStreet()-1) * 2, (this.getAvenue()-1) * 2,temp);
+
             if (nextToABeeper()){
                 this.sharedGrid.addBeeper(this.getStreet(), this.getAvenue());
             }
